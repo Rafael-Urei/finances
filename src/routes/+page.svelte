0082d00cf187d78bf0fe75db4a31fac6c0258a94
rafael-atendo <rafael.souza@atendo.com>
2024-10-12 20:17:25 -0300

    feat[TES-12]: Add redirect when user tries to navigate to home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { redirect } from '@sveltejs/kit';
+
+	if (browser) {
+		goto('/my');
+	} else {
+		redirect(302, '/my');
+	}
+</script>
